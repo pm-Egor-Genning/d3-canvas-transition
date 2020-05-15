2fef1c040366e034e83e0332a76c887b1163c1f2
Egor Genning <egor.genning@devexperts.com>
2020-05-15 13:20:19 +0700

    arcs

--- src/tags/circle.js
+++ src/tags/circle.js
@@ -1,37 +1,42 @@
-import {sizeTags} from '../size';
+import { sizeTags } from '../size';
 
-var tau = 2 * Math.PI;
+const tau = 2;
 
-export default function (node, stroke, fill, point) {
-    var attrs = node.attrs,
-        ctx = node.context,
-        f = node.factor;
-    ctx.beginPath();
-    ctx.arc(
-        f*(attrs['$cx'] || 0),
-        f*(attrs['$cy'] ||  0),
-        f*(attrs['$r'] || 0),
-        0, tau
-    );
-    ctx.closePath();
-    if (stroke) {
-      const dash = attrs.get('stroke-dasharray');
-      if (dash) {
-        ctx.setLineDash(dash.split(','));
-      }
-      ctx.stroke();
+export default function(node, stroke, fill, point) {
+  const attrs = node.attrs,
+    ctx = node.context,
+    f = node.factor;
+  ctx.beginPath();
+  ctx.arc(
+    f * (attrs.get('cx') || 0),
+    f * (attrs.get('cy') || 0),
+    f * (attrs.get('r') || 0),
+    attrs.get('startAngle') || 0,
+    (attrs.get('endAngle') || tau) * Math.PI,
+    attrs.get('anticlockwise') || false
+  );
+  ctx.closePath();
+  if (stroke) {
+    const dash = attrs.get('stroke-dasharray');
+    if (dash) {
+      ctx.setLineDash(dash.split(','));
     }
-    if (fill) ctx.fill();
-    if (point && ctx.isPointInPath(point.x, point.y)) point.nodes.push(node);
+    ctx.stroke();
+  }
+  if (fill) {
+    ctx.fill();
+  }
+  if (point && ctx.isPointInPath(point.x, point.y)) {
+    point.nodes.push(node);
+  }
 }
 
-
-sizeTags.circle = function (node) {
-    var r = node.factor*(node.attrs.get('r') || 0);
-    return {
-        x: node.factor*(node.attrs.get('cx') || 0) - r,
-        y: node.factor*(node.attrs.get('cy') || 0) - r,
-        width: 2*r,
-        height: 2*r
-    };
+sizeTags.circle = function(node) {
+  const r = node.factor * (node.attrs.get('r') || 0);
+  return {
+    x: node.factor * (node.attrs.get('cx') || 0) - r,
+    y: node.factor * (node.attrs.get('cy') || 0) - r,
+    width: 2 * r,
+    height: 2 * r
+  };
 };
